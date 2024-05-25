2eeddac61661f1a230645f56a9f52217e826b36b
Philipp-Teodor Gyarmati <philipp.gyarmati@gmail.com>
2024-05-25 17:20:21 +0300

    Added B5: Validations (check if the car is used when creating an order, check if the begin time is valid (not greater than end time), check that user does not exceed the number of reservations (5) he can have); automated assigning the order numbers)

--- Models/Order.cpp
+++ Models/Order.cpp
@@ -176,3 +176,57 @@ void Order::totalSumOfATimeInterval(std::list<Order> repository, tm time, std::s
 
         }
 }
+
+
+
+bool Order::callAllValidationFunctions(Car car, std::list<Order> repository, tm begin, tm end,
+                                             std::string status, User user) {
+    if (checkIfCarIsAlreadyUsed(car,repository,begin,end)) {std::cout<<"\nError: Car is already used on specified date; cannot create order\n"; return false;}
+
+    if (!checkIfBeginIsSmallerOrEqualEnd(begin,end)) {std::cout<<"\nError: Begin time surpasses end time\n"; return false;}
+
+    if (!userHasLessThanFiveReservations(user,status,repository)) {std::cout<<"\nError: number of reservations cannot exceed 5\n"; return false;}
+
+    orderNumber = determineOrderNumber(repository);
+
+    return true;
+}
+
+bool Order::checkIfCarIsAlreadyUsed(Car car, std::list<Order> repository, tm begin, tm end) {
+    for (Order obj: repository)
+    {
+        if (obj.car.licensePlate == car.licensePlate)  //an order has been found using the same car; check if the new order wants to use it in an already occupied time period
+            if (&end >= obj.begin && &end <= obj.end || &begin <= obj.end && &begin >= obj.begin)
+                return true;  //car is used, cannot create order
+    }
+    return false;
+}
+
+bool Order::checkIfBeginIsSmallerOrEqualEnd(tm begin, tm end) {
+    if (&begin <= &end) return true;
+    return false;
+}
+
+bool Order::userHasLessThanFiveReservations(User user, std::string status, std::list<Order> repository) {
+    if (status != "Reservation") return true;  //order is not a reservation, will not be problematic
+
+    int ct=0;
+    for (Order obj: repository)
+    {
+        if (obj.user.getUserEmail() == user.getUserEmail())  //cant have duplicate emails; users can be identified this way then
+            ct++;
+    }
+
+    if (ct >= 5) return false;  //cannot make order; user reached maximum amount of reservations
+
+    return true;
+}
+
+int Order::determineOrderNumber(std::list<Order> repository) {
+    int maxi = 0;
+    for (Order obj: repository)
+        if (obj.orderNumber > maxi)
+            maxi = obj.orderNumber;
+
+    return maxi + 1;
+}
