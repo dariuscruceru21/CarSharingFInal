3db666ae44c5bd9d933292e20bff1a25e76e5217
OOP <roeminovici@gmail.com>
2024-05-25 19:19:06 +0300

    added employee array to Employee repository

--- Repository/EmployeeRepository.h
+++ Repository/EmployeeRepository.h
@@ -4,17 +4,19 @@
 
 #ifndef CARSHARING_EMPLOYEEREPOSITORY_H
 #define CARSHARING_EMPLOYEEREPOSITORY_H
-
+#include <vector>
+#include "Employee.h"
+using namespace std;
 
 class EmployeeRepository {
+private:
+    std::vector<Employee> employees;
+
+
 public:
-    EmployeeRepository() {
+    EmployeeRepository() {}
 
-    }
-
-    ~EmployeeRepository() {
-
-    }
+    ~EmployeeRepository() {}
 
     void createEmployee();
 
