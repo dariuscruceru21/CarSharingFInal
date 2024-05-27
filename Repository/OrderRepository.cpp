2d830054357cada96679da805f6923df0c68bab1
Philipp-Teodor Gyarmati <philipp.gyarmati@gmail.com>
2024-05-27 20:05:05 +0300

    1. Moved Validations back to Order (from OrderRepository) 2. Fixed Errors in Order & OrderRepository

--- Repository/OrderRepository.cpp
+++ Repository/OrderRepository.cpp
@@ -1,99 +1,48 @@
 #include "OrderRepository.h"
 
-void OrderRepository::showAllOrdersInASpecificTimeInterval(std::list<Order> repository, tm start, tm end) {
+std::list<Order> OrderRepository::showAllOrdersInASpecificTimeInterval(std::list<Order> repository, tm start, tm end) {
     std::list<Order> orderList;
     for (Order obj: repository)
     {
-        if (obj.start >= &start && obj.end <= &end)   //object is to be written on the screen; add to orderList
+        if (obj.getStart().tm_year >= start.tm_year && obj.getStart().tm_mon >= start.tm_mon && obj.getStart().tm_mday >= start.tm_mday)
+            if (obj.getEnd().tm_year <= end.tm_year && obj.getEnd().tm_mon <= end.tm_mon && obj.getEnd().tm_mday <= end.tm_mday)
         {
             orderList.push_back(obj);
         }
     }
 
-    orderList.sort(Order::compareByTotalPrice);
+    orderList.sort(OrderRepository::compareByTotalPrice);
 
-    std::cout<<"All orders between the date of "<<&start<<" and "<<&end<<":\n";
-    for (Order i: orderList)
-    {
-        std::cout<<orderNumber<<": price-"<<totalCost<<", car-"<<car.brand<<", user-"<<user.getUserLastName()<<", employee-"<<employee.getUserLastName();
-    }
+//    std::cout<<"All orders between the date of "<<&start<<" and "<<&end<<":\n";
+//    for (Order obj: orderList)
+//    {
+//        std::cout<<obj.getOrderNr()<<": price-"<<obj.getMoney()<<", car-"<<obj.getCar().getBrand()<<", customer-"<<obj.getCustomer().getSurname()<<", employee-"<<obj.getEmployee().getSurname();
+//    }
+    return orderList;
 }
 
 Order OrderRepository::findOrderByID(std::list<Order> repository, int orderNr) {
     for (Order obj: repository)
     {
-        if (obj.orderNumber == orderNr)
+        if (obj.getOrderNr() == orderNr)
             return obj;
     }
 }
 
-void OrderRepository::totalSumOfATimeInterval(std::list<Order> repository, tm time, std::string type) {
+void OrderRepository::totalSumOfATimeInterval(std::list<Order> repository, tm time, std::string type) { //type: either month or year
     if (type == "month")
         for (Order obj: repository)
         {
-            if (obj.start->tm_mon <= time.tm_mon && obj.end->tm_mon >= time.tm_mon) //if the month is included in the order's time interval
-                std::cout<<orderNumber<<": price-"<<totalCost<<", car-"<<car.brand<<", user-"<<user.getUserLastName()<<", employee-"<<employee.getUserLastName();
+            if (obj.getStart().tm_mon <= time.tm_mon && obj.getEnd().tm_mon >= time.tm_mon) //if the month is included in the order's time interval
+                std::cout << obj.getOrderNr() << ": price-" << obj.getMoney() << ", car-" << obj.getCar().getBrand() << ", customer-" << obj.getCustomer().getSurname() << ", employee-" << obj.getEmployee().getSurname();
         }
     else
         for (Order obj: repository)
         {
-            if (obj.start->tm_year >= time.tm_year && obj.end->tm_year <= time.tm_year) //if the time is included in a year
-                std::cout<<orderNumber<<": price-"<<totalCost<<", car-"<<car.brand<<", user-"<<user.getUserLastName()<<", employee-"<<employee.getUserLastName();
+            if (obj.getStart().tm_year >= time.tm_year && obj.getEnd().tm_year <= time.tm_year) //if the order time is included in the given year
+                std::cout<<obj.getOrderNr()<<": price-"<<obj.getMoney()<<", car-"<<obj.getCar().getBrand()<<", user-"<<obj.getCustomer().getSurname()<<", employee-"<<obj.getEmployee().getSurname();
 
         }
-}
-
-
-bool OrderRepository::callAllValidationFunctions(Car car, std::list<Order> repository, tm begin, tm end,
-                                                 std::string status, User user) {
-    if (checkIfCarIsAlreadyUsed(car,repository,begin,end)) {std::cout<<"\nError: Car is already used on specified date; cannot create order\n"; return false;}
-
-    if (!checkIfBeginIsSmallerOrEqualEnd(begin,end)) {std::cout<<"\nError: Begin time surpasses end time\n"; return false;}
-
-    if (!userHasLessThanFiveReservations(user,status,repository)) {std::cout<<"\nError: number of reservations cannot exceed 5\n"; return false;}
-
-    orderNumber = determineOrderNumber(repository);
-
-    return true;
-}
-
-bool OrderRepository::checkIfCarIsAlreadyUsed(Car car, std::list<Order> repository, tm begin, tm end) {
-    for (Order obj: repository)
-    {
-        if (obj.car.licensePlate == car.licensePlate)  //an order has been found using the same car; check if the new order wants to use it in an already occupied time period
-            if (&end >= obj.start && &end <= obj.end || &begin <= obj.end && &begin >= obj.start)
-                return true;  //car is used, cannot create order
-    }
-    return false;
-}
-
-bool OrderRepository::checkIfBeginIsSmallerOrEqualEnd(tm begin, tm end) {
-    if (&begin <= &end) return true;
-    return false;
-}
-
-bool OrderRepository::userHasLessThanFiveReservations(User user, std::string status, std::list<Order> repository) {
-    if (status != "Reservation") return true;  //order is not a reservation, will not be problematic
-
-    int ct=0;
-    for (Order obj: repository)
-    {
-        if (obj.getUser().getUserEmail() == user.getUserEmail())  //cant have duplicate emails; users can be identified this way then
-            ct++;
-    }
-
-    if (ct >= 5) return false;  //cannot make order; user reached maximum amount of reservations
-
-    return true;
-}
-
-int OrderRepository::determineOrderNumber(std::list<Order> repository) {
-    int maxi = 0;
-    for (Order obj: repository)
-        if (obj.getOrderNr() > maxi)
-            maxi = obj.getOrderNr();
-
-    return maxi + 1;
 }
 
 
@@ -118,7 +67,7 @@ void OrderRepository::saveOrder(Order obj) {
         return;
     }
 
-    for (const auto &obj1 : repo) {
+    for (auto &obj1 : repo) {
         g << obj1.toCSV() << std::endl;
     }
 
@@ -145,7 +94,7 @@ void OrderRepository::deleteOrder(Order obj) {
         return;
     }
 
-    for (const auto &obj1 : repo) {
+    for (auto &obj1 : repo) {
         g << obj1.toCSV() << std::endl;
     }
 
@@ -181,7 +130,7 @@ void OrderRepository::updateOrder(Order obj) {
         return;
     }
 
-    for (const auto &obj1 : repo) {
+    for (auto &obj1 : repo) {
         g << obj1.toCSV() << std::endl;
     }
 
@@ -284,5 +233,4 @@ Order OrderRepository::searchOrder(int orderID) {
     }
     f.close();
     std::cout<<"Error: object does not exist";
-    return;
 }
